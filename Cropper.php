5c05fe763abfbf2149e5f860449e75011322d34e
Carsten <carsten-tetzlaff@web.de>
2015-09-14 15:19:00 +0200

    Update Cropper.php

--- Cropper.php
+++ Cropper.php
@@ -8,6 +8,7 @@ use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\helpers\Json;
 use yii\helpers\Url;
+use yii\web\View;
 use yii\web\JsExpression;
 
 /**
@@ -33,10 +34,17 @@ class Cropper extends Widget {
     public $imageOptions = [];
     /** @var array Default cropper options https://github.com/fengyuanchen/cropper/blob/master/README.md#options */
     public $defaultPluginOptions = [
-        'strict' => true,
-        'autoCropArea' => 1,
-        'checkImageOrigin' => false,
-        'zoomable' => false,
+        'strict' 			=> false,
+        'autoCropArea' 		=> 0.75,
+        'checkImageOrigin' 	=> false,
+        'zoomable' 			=> true,
+/*
+  		'guides' 			=> false,
+  		'highlight' 		=> false,
+  		'dragCrop'			=> false,
+  		'cropBoxMovable'	=> false,
+  		'cropBoxResizable'	=> false,
+*/  		        
     ];
     /** @var array Additional cropper options https://github.com/fengyuanchen/cropper/blob/master/README.md#options */
     public $pluginOptions = [];
@@ -48,27 +56,31 @@ class Cropper extends Widget {
    /** @var array Additional cropper options https://github.com/fengyuanchen/cropper/blob/master/README.md#options */
     public $cropButtons = [];
 
+    /*
+     *  Attention! I had to rename data-method to data-cropper-method cause of incompatible problems with pjax
+     */
+    
     public $defaultCropButtons = [
-	[
-		'move' => [
-			['data-method' => 'move','data-option'=>'setDragMode','title'=>'Move'],
+		[
+			'move' => [
+				['data-cropper-method' => 'move','data-option'=>'setDragMode','title'=>'Move','icon'=>'fa fa-arrows'],
+			],
+			'crop' => [
+				['data-cropper-method' => 'crop','data-option'=>'setDragMode','title'=>'Crop','icon'=>'fa fa-crop'],
+			],
 		],
-		'crop' => [
-			['data-method' => 'crop','data-option'=>'setDragMode','title'=>'Crop'],
-		],
-	],
-	[
+		[
     		'zoom' => [
-			['data-method' => 'zoom','data-option'=>'0.1', 'title'=>'Zoom in'],
-			['data-method' => 'zoom','data-option'=>'-0.1', 'title'=>'Zoom out'],
+				['data-cropper-method' => 'zoom','data-option'=>'0.1', 'title'=>'Zoom in','icon'=>'fa fa-search-plus'],
+				['data-cropper-method' => 'zoom','data-option'=>'-0.1', 'title'=>'Zoom out','icon'=>'fa fa-search-minus'],
+			],
 		],
-	],
-	[
-		'reset' => [
-			['data-method' => 'reset','title'=>'Reset'],
-		],
-		'upload' => true,
-	]
+		[
+			'reset' => [
+				['data-cropper-method' => 'reset','title'=>'Reset','icon'=>'fa fa-refresh'],
+			],
+			'upload' => true,
+		]
     ];    
 
     /**
@@ -77,6 +89,25 @@ class Cropper extends Widget {
      * Example: '@backend/path/to/view'
      */
     public $pathToView = 'cropper';
+
+    /**
+     * @var string thumbHolder is a class for elements with images.
+     * 
+     */
+    public $thumbHolder = 'thumbHolder';
+    
+    
+    /**
+     * @var boolean The size of the cropper inherits form the size of it´s parent element. In case I use X-Tabs I have to initialize the cropper again
+     * 
+     */
+    public $initOnVisible = false;
+
+    /**
+     * @var string Id of hidden input filed
+     */
+    private $imageData = 'cropImageData';    
+    
     /**
      * @inheritdoc
      */
@@ -91,6 +122,8 @@ class Cropper extends Widget {
         $this->pluginOptions = ArrayHelper::merge($this->defaultPluginOptions, $this->pluginOptions);
         $this->imageOptions = ArrayHelper::merge($this->defaultImageOptions, $this->imageOptions);
 
+        $this->cropButtons =  ArrayHelper::merge($this->defaultCropButtons, $this->cropButtons);
+        
         // Set additional cropper js-options
         if (!empty($this->aspectRatio)) {
             $this->pluginOptions['aspectRatio'] = $this->aspectRatio;
@@ -98,7 +131,7 @@ class Cropper extends Widget {
         
         parent::init();
         
-        WidgetAset::register($this->view);
+        WidgetAsset::register($this->view);
 
         $this->options = array_merge([
             'accept' => 'image/*',
@@ -106,7 +139,51 @@ class Cropper extends Widget {
         
         $optionsCropper = Json::encode($this->pluginOptions);
 
-        $js = "$('#{$this->id}').cropper({$optionsCropper});";
+	    $js = "jQuery('#{$this->id}').myCropper({$optionsCropper});";
+	   	// for cropping on show use: $js.= "jQuery('#{$this->id}').on('built.cropper cropend.cropper', function(e) {
+	   	$js.= "jQuery('#{$this->id}').on('cropend.cropper', function(e) { 
+	        var data = jQuery(this).cropper('getData');
+	        //var original= jQuery(this).cropper('getImageData');
+			//jQuery.extend(data,original) 
+	        var value = JSON.stringify(data);
+	        $('#{$this->imageData}').val(value); 
+		});";
+	    
+	   	$jsCrop = $js;
+   	
+	    if($this->initOnVisible !== false) {
+
+	    	
+        	$js = "var parent = jQuery('#{$this->id}').closest('div[class^=\"tab-pane\"]');";
+        	$js.= "var parentId = parent.attr('id');";
+
+        	$js.= "jQuery('a[href=\'#'+parentId+'\']').on('shown.bs.tab', function (e) {
+        		$jsCrop;	
+	        });";
+        }
+
+    	if(!empty($this->image)) {
+	   		$js.="jQuery('#{$this->id}').cropper('destroy');";
+	   		$js.="jQuery('#{$this->id}').attr('src',jQuery(this).attr('{$this->image}'));";
+	   		$js.= "jQuery('.{$this->thumbHolder} > img').on('click', function() {
+	   				jQuery('#imageHolder').hide();
+	   				jQuery('#imageUploader').show();	
+	   		}) ;";
+	   	}        
+        
+	   	/*
+	   	 * Init cropper for existing images
+	   	 * image must be child of an element with class=$this->thumbHulder 
+	   	 */
+	   	$js.= "jQuery('.{$this->thumbHolder} > img').on('click', function() {
+	   		jQuery('#{$this->id}').cropper('destroy');
+	   		jQuery('#{$this->id}').attr('src',jQuery(this).attr('data-src'));
+	   		jQuery('.image-src').val(jQuery(this).attr('data-src'));
+	   		$jsCrop;
+    	}) ;"; 
+
+
+        
         $this->view->registerJs($js, View::POS_READY);
 
     }
@@ -128,3 +205,4 @@ class Cropper extends Widget {
     }    
     
 }
+?>
